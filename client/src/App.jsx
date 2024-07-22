b023280fc0b16c2a5b09e25ffee0f05e98066459
Tobi-Daniel <oloyebmv@gmail.com>
2024-07-22 14:49:45 +0100

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage";
+import About from "./pages/About/About";
+import SignIn from "./pages/Auth/SignIn";
+import SignUp from "./pages/Auth/SignUp";
+import Dashboard from "./pages/Admin/Dashboard";
+import BlogPostPage from "./pages/Blog/BlogPostPage";
 
 const App = () => {
   return (
-    <div className='bg-slate-500'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/blog-post" element={<BlogPostPage/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
